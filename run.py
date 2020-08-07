5c365226ca36f282011cb6963cef198837f5ac12
Damien Sprumont <sprumontdamien@gmail.com>
2020-08-07 20:28:43 +0200

    fix config.json path

--- run.py
+++ run.py
@@ -40,8 +40,7 @@ def main(argv):
         conn.job.update(progress=0,
                         statusComment="Loading segmentation model..")
 
-        with open(os.path.join(
-                base_path, "models/resnet50b_fpn256/config.json")) as f:
+        with open("/models/resnet50b_fpn256/config.json") as f:
             config = json.load(f)
         model = FPN.build_resnet_fpn(
             name=config['name'],
